40c49e308dfc6f44c90a3d82840d1ec7259cacc7
abhijit kumar singh <abhijitkumarsingh.cse58@gmail.com>
2025-01-26 22:59:06 +0530

    second commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="bg-[#EDF9FD] min-h-screen flex justify-between items-start flex-wrap p-5">
       <button className="bg-[#FEFCE8] hover:bg-brand-100 transition-all rounded-full button h-[52px] gap-2 px-10 shadow-drop-1 flex items-center text-center font-semibold text-lg p-10">
-        <Link to="https://school-three-mocha.vercel.app/" target="_blank">
+        <Link to="https://sudamasol-admindashboard.vercel.app/" target="_blank">
           Admin Dashboard
         </Link>
       </button>
